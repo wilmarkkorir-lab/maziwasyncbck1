4e1912cf4d038b1c290ddf77088135482f6938cb
wilmarkkorir-lab <wilmarkkorir@gmail.com>
2026-07-23 12:28:13 +0300

    add STATIC_ROOT

--- maziwasynckbck/settings.py
+++ maziwasynckbck/settings.py
@@ -302,8 +302,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
