98559ed0b92e295c2cf390492fdde6a02cc3b6cc
wilmarkkorir-lab <wilmarkkorir@gmail.com>
2026-07-30 22:04:23 +0300

    pub7

--- maziwasynckbck/settings.py
+++ maziwasynckbck/settings.py
@@ -93,10 +93,10 @@
 # # https://docs.djangoproject.com/en/4.2/ref/settings/
 
 # # DATABASES = {
-# #     'default': {
-# #         'ENGINE': 'django.db.backends.sqlite3',
-# #         'NAME': BASE_DIR / 'db.sqlite3',
-# #     }
+# #     # 'default': {
+# #     #     'ENGINE': 'django.db.backends.sqlite3',
+# #     #     'NAME': BASE_DIR / 'db.sqlite3',
+# #     # }
 # # }
 
 # import socket
@@ -326,7 +326,9 @@ SIMPLE_JWT = {
 # CORS
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
